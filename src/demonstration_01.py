1fb8e58bfbd9d16dbc5a1cbe39c8f34ab386839c
spazdrum <spazdrum@gmail.com>
2020-10-29 23:26:39 -0400

    Initial commit

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -12,9 +12,65 @@ Examples:
 Notes:
 - Return "invalid" if n exceeds the length of the list.
 - Return an empty list if n == 0.
+
+(reverse)
+Write a function that retrieves the first n elements from a list
+- Return "invalid" if n exceeds the length of the list.
+- Return an empty list if n == 0.
+
+- [0 to n] python we have a slice (takes a sub section of a list and returns that data as a list)
+- [start inclusive : end exclusive]
+- in JS / TS '.slice()' method
+
+# Check if n is greater than the length of the list
+    # then return 'invalid' to the caller
+# otherwise if n is equal to zero
+    # return an empty list
+
+# otherwise 
+    # return the slice of [0 : n]
+
+(forward)
+Write a function that retrieves the first n elements from a list
+- Return "invalid" if n exceeds the length of the list.
+- Return an empty list if n == 0.
+
+- [0 to n] python we have a slice (takes a sub section of a list and returns that data as a list)
+- [start inclusive : end exclusive]
+- in JS / TS '.slice()' method
+
+# Check if n is greater than the length of the list
+    # then return 'invalid' to the caller
+# otherwise if n is equal to zero
+    # return an empty list
+
+# otherwise 
+    # Return the slice of [ length of the list - (n - 1) : length of list + 1 ]
+
+
+
 """
 
 
 def last(a, n):
-    # Your code here
+    # Check if n is greater than the length of the list
+    if n > len(a):
+        # then return 'invalid' to the caller
+        return "invalid"
+    # otherwise if n is equal to zero
+    elif n == 0:
+        #return an empty list
+        return []
+
+    #otherwise
+    else:
+        #return the slice of [ length of the list - n : length of list + 1]
+        # return a[len(a) - n : len(a) + 1]
+        # return a slice of list[-n : ]
+        return a[-n:]
+
+print(last([1, 2, 3, 4, 5], 1)) # ➞ [5]
+print(last([4, 3, 9, 9, 7, 6], 3)) # ➞ [9, 7, 6]
+print(last([1, 2, 3, 4, 5], 7)) # ➞ "invalid"
+print(last([1, 2, 3, 4, 5], 0)) # ➞ []
 
--- src/demonstration_04.py
+++ src/demonstration_04.py
@@ -20,9 +20,64 @@ Examples:
 Notes:
 - The sentence always starts with "Make me".
 - Try to solve this without using conditional statements like if/else.
+
+- We might want to use a dictionary to store the key value pairs
+
+{
+    "smile": ":)"
+    "grin": ":D"
+    "sad": ":("
+    "mad": ">:|"
+}
+- iterate over the hash table items extrapolating the key and value of each item
+- user a string '.replace' to replace the substring of the key with the value
+
 """
 
 
 def emotify(txt):
-    # Your code here
+    # first pass
+    # store the key value pairs of the required data for swapping out the text to emoticons
+    # we will store this data in a hash table (dictionary / object)
+    # data = {
+    #     "smile": ":)",
+    #     "grin": ":D",
+    #     "sad": ":(",
+    #     "mad": ">:|"
+    # }
+
+    # iterate over the hash table items extracting the kay and value
+    # for k, v in data.items():
+        # set an updated string using the '.replace' method 
+        # to return each new txt with the old substring substituted for the old one
+        # txt = txt.replace(k, v)
+
+    # return our new txt
+    # return txt
+
+    # second pass
+        # store the key value pairs of the required data for swapping out the text to emoticons
+    # we will store this data in a hash table (dictionary / object)
+    data = {
+        "smile": ":)",
+        "grin": ":D",
+        "sad": ":(",
+        "mad": ">:|"
+    }
+
+    # get the substring of txt from the start to index 7 (8 - 1) indices in [ :8] set it as start_of_string -> "Make me "
+    start_of_string = txt[:8]
+    # get the data at 8 in from the start of the string [8: ] set it as end_of_string -> "smile"
+    end_of_string = txt[8:]
+    # concatonate start_of_string with value from data using end_of_string -> "smile" -> ":)" as the key and set them to full_string
+    full_string = start_of_string + data[end_of_string]
+    # return full_string]
+    return full_string
+
+
+
+print(emotify("Make me smile")) # ➞ "Make me :)"
+print(emotify("Make me grin")) # ➞ "Make me :D"
+print(emotify("Make me sad"))  # ➞ "Make me :("
+print(emotify("Make me mad")) # ➞ "Make me >:|"
 
